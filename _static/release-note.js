6849c33d32228b1ba38ac7733723b2829528191c
redfoxtshimomura <shimomura.tadashi@redfox.co.jp>
2014-11-19 17:56:49 +0900

    Push for publishing.

---
+++ _static/release-note.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+    $("a.internal:has('em')").each(function(i,v) {
+        var url = $(v).attr("href");
+        var ref_id = url.match(/(#.+$)/)[0];
+        if (typeof url == "undefined" || typeof ref_id == "undefined") {
+            return;
+        }
+        var dummy_ = $("<iframe />").attr("src",url).attr("id","_link_").css("width","0px").appendTo("body");
+        if (typeof dummy_ == "undefined") {
+            return;
+        }
+        setTimeout(function(){
+	        var cont_ = dummy_.contents().find(ref_id);
+	        var cont_elm = dummy_.contents().find(ref_id).next();
+	        var cont_obj = $("<div/>").append(cont_.clone()).append(cont_elm.clone());
+	        $(v).balloon({ 
+	            contents: cont_obj,
+	            position:"top right",
+				css: {
+				    "font-size": "60%",
+				    "opacity": "0.95",
+				    border: 'solid 4px #5baec0',
+				    padding: '10px',
+				    fontWeight: 'bold',
+				    lineHeight: '3',
+				    backgroundColor: '#666',
+				    color: '#fff'
+				} });
+	        $("#_link_").remove();
+        },1000);
+        if (location.hash != "") {
+           $.smoothScroll({ 
+               scrollElement:null,
+               scrollTarget:'location.hash'
+           }); 
+        }
+       
+    });
+
+});
